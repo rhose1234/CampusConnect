f87ec3a2b041607a6c203067fa3fe561ba99ef27
olami20-dev <haleemoladejo@gmail.com>
2025-09-11 16:27:28 +0100

    homepage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import { Route, Routes } from 'react-router-dom'
-import Homepage from './Pages/Homepage'
-import Events from './Pages/Events/Events'
-import Navbar from './Components/Navbar'
-
-
-
+import React, { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Homepage from "./Pages/Homepage";
+import Events from "./Pages/Events/Events";
+import Bookmarks from "./Pages/Bookmark/Bookmark";
 
 export default function App() {
+  const [bookmarks, setBookmarks] = useState([]);
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch("/Data/EventListing.json")
+      .then((res) => res.json())
+      .then((data) => setEvents(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem("bookmarkedEvents")) || [];
+    setBookmarks(stored);
+  }, []);
+
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route index element={<Homepage/>} />
-      <Route path="/events" element={<Events/>} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route
+          index
+          element={<Homepage bookmarks={bookmarks} setBookmarks={setBookmarks} />}
+        />
+        <Route
+          path="/events"
+          element={<Events events={events} bookmarks={bookmarks} setBookmarks={setBookmarks} />}
+        />
+        <Route
+          path="/bookmarks"
+          element={<Bookmarks events={events} bookmarks={bookmarks} />}
+        />
+      </Routes>
     </>
-  )
+  );
 }
