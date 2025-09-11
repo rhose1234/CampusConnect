c304dc2f29151b89cd96785467ab1840ebaf5c63
rhose1234 <marvelousrhose@gmail.com>
2025-09-11 11:59:49 +0100

    events page in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Homepage from './Pages/Homepage'
+import Events from './Pages/Events/Events'
 
 
 export default function App() {
   return (
     <Routes>
-      <Route index element={<Homepage />} />
+      <Route index element={<Homepage/>} />
+      <Route path="/events" element={<Events/>} />
      
     </Routes>
   )
