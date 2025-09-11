c97dad59bc58dacd38633f9ea80935af336269d9
lilnoodle23 <davvidggg@gmail.com>
2025-09-11 15:57:07 +0100

    david's branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Homepage from './Pages/Homepage'
-
+import About from './Pages/about/About'
 
 export default function App() {
   return (
     <Routes>
       <Route index element={<Homepage />} />
+      <Route path="/about" element={<About/>}/>
+      
      
     </Routes>
   )
