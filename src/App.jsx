bff447ccb48e3b3efc32f21137135a3b491c5949
DevFerra19 <emmanuelakanbi45@gmail.com>
2025-09-12 10:05:11 +0100

    Contact, Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Homepage from './Pages/Homepage'
+import Contact from './Pages/Contact'
+import Gallery from './Pages/Gallery'
 
 
 export default function App() {
   return (
     <Routes>
-      <Route index element={<Homepage />} />
-     
+      <Route path="/" element={<Homepage />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/gallery" element={<Gallery />} />
     </Routes>
   )
 }
